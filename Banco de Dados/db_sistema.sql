a772d4808dae3bc61eabca9f29057625661769b2
Juniinho Chiodi <juninho.junirj@gmail.com>
2022-05-25 23:56:57 -0300

    .

--- Banco de Dados/db_sistema.sql
+++ Banco de Dados/db_sistema.sql
@@ -1,28 +1,73 @@
--- MySQL Workbench Synchronization
--- Generated: 2022-05-25 22:23
--- Model: New Model
--- Version: 1.0
--- Project: Name of the project
--- Author: Usuario
+-- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-ALTER SCHEMA `db_sistema`  DEFAULT CHARACTER SET utf8  DEFAULT COLLATE utf8_general_ci ;
+-- -----------------------------------------------------
+-- Schema db_sistema
+-- -----------------------------------------------------
 
-ALTER TABLE `db_sistema`.`pessoa` 
-CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
+-- -----------------------------------------------------
+-- Schema db_sistema
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `db_sistema` DEFAULT CHARACTER SET utf8 ;
+USE `db_sistema` ;
 
-ALTER TABLE `db_sistema`.`produto` 
-CHARACTER SET = utf8 , COLLATE = utf8_general_ci ,
-CHANGE COLUMN `descricao` `descricao` TEXT NOT NULL AFTER `nome`;
+-- -----------------------------------------------------
+-- Table `db_sistema`.`pessoa`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_sistema`.`pessoa` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NOT NULL,
+  `rg` VARCHAR(45) NOT NULL,
+  `cpf` CHAR(11) NOT NULL,
+  `data_nascimento` DATE NOT NULL,
+  `email` VARCHAR(255) NULL,
+  `telefone` VARCHAR(11) NULL,
+  `endereco` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `cpf_UNIQUE` (`cpf` ASC) VISIBLE,
+  UNIQUE INDEX `rg_UNIQUE` (`rg` ASC) VISIBLE)
+ENGINE = InnoDB;
 
-ALTER TABLE `db_sistema`.`categoria` 
-CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
 
-ALTER TABLE `db_sistema`.`funcionario` 
-CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
+-- -----------------------------------------------------
+-- Table `db_sistema`.`produto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_sistema`.`produto` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `descricao` TEXT NOT NULL,
+  `preco` DOUBLE NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `db_sistema`.`categoria`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_sistema`.`categoria` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `descricao` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `db_sistema`.`funcionario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_sistema`.`funcionario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `rg` VARCHAR(45) NOT NULL,
+  `cpf` CHAR(11) NOT NULL,
+  `data_nascimento` DATE NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `telefone` VARCHAR(11) NOT NULL,
+  `endereco` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
